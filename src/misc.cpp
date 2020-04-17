71818ab196f9e16bccdf02912337d01b42a16325
Ahmet Burak Gürsu <47427465+VezirChess@users.noreply.github.com>
2020-04-17 09:14:19 +0300

    Update misc.cpp

--- src/misc.cpp
+++ src/misc.cpp
@@ -122,7 +122,7 @@ const string engine_info(bool to_uci) {
   string month, day, year;
   stringstream ss, date(__DATE__); // From compiler, format is "Sep 21 2008"
 
-  ss << "BrainLearn Monte-Carlo " << Version << setfill('0');
+  ss << "BrainLearn MCTS " << Version << setfill('0');
 
   if (Version.empty())
   {
